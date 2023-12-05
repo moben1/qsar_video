a5721bf392e4955d3c65f5fc601466783345e1d5
moben1 <aekmedbenabbassi@gmail.com>
2023-12-05 13:56:20 -0500

    adjust times

--- models.py
+++ models.py
@@ -3,22 +3,22 @@ from manim import *
 
 class Models(Scene):
     def construct(self):
-        training_data = Tex("Apprentissage ensembliste", font_size=28, color=BLUE).to_edge(UP)
+        title = Tex("Apprentissage ensembliste", font_size=28, color=BLUE).to_edge(UP)
         subsets_list = []
         for i in range(3):
             t = f"{i + 1}" if i < 2 else "n"
             subsets_list.append(Tex("$Sous{-}ensemble_{" + t + "}$", font_size=28))
         subsets = VGroup(*subsets_list)
-        subsets.arrange(RIGHT, buff=2).next_to(training_data, DOWN, buff=0.5)
+        subsets.arrange(RIGHT, buff=2).next_to(title, DOWN, buff=0.5)
 
-        self.play(Write(training_data))
-        self.play(LaggedStart(*[Write(subset) for subset in subsets], lag_ratio=0.5))
+        self.play(Write(title), run_time=0.3)
+        self.play(LaggedStart(*[Write(subset) for subset in subsets], lag_ratio=0.1))
         self.wait()
 
         tree_positions = [4 * LEFT + 2 * UP, 2 * UP, 4 * RIGHT + 2 * UP]
         trees = VGroup(*[self.create_decision_tree(pos) for pos in tree_positions])
         for tree in trees:
-            self.play(FadeIn(tree, shift=UP), run_time=2)
+            self.play(FadeIn(tree, shift=UP), run_time=1)
         self.wait()
 
         paths = [
@@ -27,29 +27,20 @@ class Models(Scene):
             [0, 14, 1, 13, 8, 12, 11]
         ]
 
-        predictions_list = []
-        for i in range(3):
-            t = f"{i + 1}" if i < 2 else "n"
-            predictions_list.append(Tex("$Prediction_{" + t + "}$", font_size=28, color=WHITE).next_to(trees[i], DOWN))
-        predictions = VGroup(*predictions_list)
-
         for i, path in enumerate(paths):
             for j in path:
-                self.play(ApplyMethod(trees[i][j].set_color, YELLOW))
-                self.wait()
+                self.play(ApplyMethod(trees[i][j].set_color, YELLOW), run_time=0.1)
+            t = f"{i + 1}" if i < 2 else "n"
+            prediction = Tex("$Prediction_{" + t + "}$", font_size=28, color=WHITE).next_to(trees[i], DOWN)
+            self.play(Write(prediction), run_time=0.5)
 
         final_prediction = MathTex("Prediction finale = \\frac{1}{T} \\sum_{i=1}^{T} Prediction_{i}",
-                                   color=WHITE, font_size=28).next_to(
-            predictions, DOWN, buff=1)
-
-        for prediction in predictions:
-            self.play(Write(prediction))
+                                   color=WHITE, font_size=28).next_to(trees[1], 3 * DOWN)
 
         self.play(Write(final_prediction))
-        self.wait()
+        self.wait(2)
 
-    def create_decision_tree(self, root_position):
-        depth = 4
+    def create_decision_tree(self, root_position, depth=4):
         tree = VGroup()
 
         def create_nodes(position, level, max_width):
